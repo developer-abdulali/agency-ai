391fac9da09fc5e2445ab8307abbc80119dcaf69
developer-abdulali <ab.ali.soomro@gmail.com>
2024-03-12 01:24:31 +0500

    added changes

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,13 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import CartSlice from "./slices/CartSlice";
 import CategorySlice from "./slices/CategorySlice";
 import SearchSlice from "./slices/SearchSlice";
-const Store = configureStore({
-  reducer: {
+
+const persistConfig = {
+  key: "appData",
+  storage: storage,
+};
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  combineReducers({
     cart: CartSlice,
     category: CategorySlice,
     search: SearchSlice,
-  },
+  })
+);
+
+const Store = configureStore({
+  reducer: persistedReducer,
 });
 
-export default Store;
+const persistor = persistStore(Store);
+
+export { Store, persistor };
