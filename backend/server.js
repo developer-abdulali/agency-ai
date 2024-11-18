0267c125f7fa195eeade0c16d0bfa36707df92da
developer-abdulali <ab.ali.soomro@gmail.com>
2024-11-18 23:17:51 +0500

    added changes in vercel.json

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,9 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(
+  cors({ origin: ["https://mern-note-app-ui.vercel.app"], credentials: true })
+);
 
 // api routes
 app.use("/api/auth", authRoutes);
