9db95583cf43602d3d7947a8e850d5f4f442a655
developer-abdulali <ab.ali.soomro@gmail.com>
2024-12-31 00:15:38 +0500

    added changes

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const connectDB = require("./config/connectDB");
 const router = require("./routes/index");
 const cookiesParser = require("cookie-parser");
-const { app, server } = require("./socket/socket");
+const { app, server } = require("./socket/index");
 
 app.use(
   cors({
