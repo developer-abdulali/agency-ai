f4200c126ca535d7c088427e16e5c0cb8cf186ff
developer-abdulali <ab.ali.soomro@gmail.com>
2024-12-31 12:46:11 +0500

    created build of client and add changes in server

--- server/index.js
+++ server/index.js
@@ -1,15 +1,50 @@
+// import express from "express";
+// import cors from "cors";
+// import connectDB from "./config/connectDB.js";
+// import cookiesParser from "cookie-parser";
+// import router from "./routes/index.js";
+// import { app, server } from "./socket/index.js";
+
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL,
+//     credentials: true,
+//   })
+// );
+// app.use(express.json());
+// app.use(cookiesParser());
+
+// const PORT = process.env.PORT || 8080;
+
+// app.get("/", (request, response) => {
+//   response.json({
+//     message: "Server running at " + PORT,
+//   });
+// });
+
+// //api endpoints
+// app.use("/api", router);
+
+// connectDB().then(() => {
+//   server.listen(PORT, () => {
+//     console.log("server running at " + PORT);
+//   });
+// });
+
 import express from "express";
 import cors from "cors";
 import connectDB from "./config/connectDB.js";
 import cookiesParser from "cookie-parser";
 import router from "./routes/index.js";
 import { app, server } from "./socket/index.js";
-
+import path from "path";
 import dotenv from "dotenv";
 dotenv.config();
 
-// const app = express();
-
+// Middleware setup
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
@@ -19,19 +54,29 @@ app.use(
 app.use(express.json());
 app.use(cookiesParser());
 
-const PORT = process.env.PORT || 8080;
+// Static file serving for frontend
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/client/build")));
 
+// Catch-all route for serving React's index.html
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+);
+
+// API endpoints
+app.use("/api", router);
+
+// Default route
 app.get("/", (request, response) => {
   response.json({
     message: "Server running at " + PORT,
   });
 });
 
-//api endpoints
-app.use("/api", router);
-
+// Connect to the database and start the server
+const PORT = process.env.PORT || 8080;
 connectDB().then(() => {
   server.listen(PORT, () => {
-    console.log("server running at " + PORT);
+    console.log("Server running at " + PORT);
   });
 });
