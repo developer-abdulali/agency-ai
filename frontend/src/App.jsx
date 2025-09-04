a972619a67b25c29e874088b90dc42da905eece8
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2025-09-05 02:07:36 +0500

    created auth component and fix auth issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,76 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-} from "@clerk/clerk-react";
+import { SignedIn, SignedOut, useAuth } from "@clerk/clerk-react";
+import { Navigate, Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
 
 const App = () => {
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if (!isLoaded) return null;
+
   return (
-    <div>
-      <header>
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </header>
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={isSignedIn ? <Home /> : <Navigate to={"/auth"} replace />}
+      />
+      <Route
+        path="/auth"
+        element={!isSignedIn ? <Auth /> : <Navigate to={"/"} replace />}
+      />
+
+      {/* <Route
+        path="/call/:id"
+        element={isSignedIn ? <CallPage /> : <Navigate to={"/auth"} replace />}
+      /> */}
+
+      <Route
+        path="*"
+        element={
+          isSignedIn ? (
+            <Navigate to={"/"} replace />
+          ) : (
+            <Navigate to={"/auth"} replace />
+          )
+        }
+      />
+    </Routes>
+
+    // <>
+    //   <SignedIn>
+    //     <Routes>
+    //       <Route path="/" element={<Home />} />
+    //       <Route path="/auth" element={<Navigate to="/" replace />} />
+    //     </Routes>
+    //   </SignedIn>
+
+    //   <SignedOut>
+    //     <Routes>
+    //       <Route path="/auth" element={<Auth />} />
+    //       <Route path="*" element={<Navigate to="/auth" replace />} />
+    //     </Routes>
+    //   </SignedOut>
+    // </>
   );
 };
 
 export default App;
+
+// first version of routing:
+// return (
+//   <>
+//     <SignedIn>
+//       <SentryRoutes>
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="/auth" element={<Navigate to={"/"} replace />} />
+//       </SentryRoutes>
+//     </SignedIn>
+
+//     <SignedOut>
+//       <SentryRoutes>
+//         <Route path="/auth" element={<AuthPage />} />
+//         <Route path="*" element={<Navigate to={"/auth"} replace />} />
+//       </SentryRoutes>
+//     </SignedOut>
+//   </>
+// );
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter } from "react-router";
 
 import App from "./App.jsx";
 
@@ -15,7 +16,9 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ClerkProvider>
   </StrictMode>
 );
